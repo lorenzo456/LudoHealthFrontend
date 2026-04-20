e6e1fd2846d90084664d1046f24cb2371280386e
lorenzo456 <lorenzo456_@hotmail.com>
2026-04-20 14:58:06 +0200

    added env

--- src/api/Apiclient.ts
+++ src/api/Apiclient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
